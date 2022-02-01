3740f0687205eaa2135d1db6382bcea9458727e6
Brian Zalewski <brian@megabyte.space>
2022-01-31 20:29:45 -0500

    🧹 chore(taskfiles): added latest taskfiles

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-
-import { parsers } from 'prettier/parser-babel'
 import { format } from 'prettier-package-json'
+import { parsers } from 'prettier/parser-babel'
 
 const parser = parsers['json-stringify']
 
+// eslint-disable-next-line functional/immutable-data, unicorn/prefer-module
 exports.parsers = {
-    'json-stringify': {
-        ...parser,
-        preprocess(text: string, options: any) {
-            const regex = new RegExp("package.*json$")
-            return regex.test(options.filepath) ? format(JSON.parse(text)) : text
-        }
+  'json-stringify': {
+    ...parser,
+    preprocess(text: string, options: any) {
+      const regex = /package.*json$/u
+
+      return regex.test(options.filepath) ? format(JSON.parse(text)) : text
     }
+  }
 }
